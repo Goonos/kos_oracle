591ddae18e74080654ae3024a28c0bc2b9b5c957
Goonos <150596178+Goonos@users.noreply.github.com>
2026-06-25 15:46:38 +0900

    Update queries.js

--- queries.js
+++ queries.js
@@ -30,6 +30,7 @@ const SQL_QUERIES = {
           `,
 
     ts02: `SELECT * FROM some_large_table WHERE condition = true;`,
+    ts03: `SELECT * FROM some_large_table WHERE condition = true;`,
     
     // 앞으로 추가될 긴 쿼리들은 여기에 계속 나열하면 됩니다.
 };
